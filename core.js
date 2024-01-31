5f5bf285eedb1dba710ca9bd62d109ee23d8c548
dangVuDinh <139780574+dangNguyenDinh@users.noreply.github.com>
2024-01-31 08:38:35 +0700

    Update core.js

--- core.js
+++ core.js
@@ -7,81 +7,76 @@ import Koopa from "./koopa.js";
 
 //main
 var a = new Mario();
-document.getElementById("mario").style.top = a.top;
+document.getElementById("mario").style.bottom = a.bottom;
 document.getElementById("mario").style.left = a.left;
+//ready step
+document.querySelector("#mario").style.display = "none";
 
-//xử lý phần nhận tín hiệu để sử dụng vũ khí
-// document.addEventListener("keydown",function(e){
-//     if(e.key === " "){
-//         a.renderMarioGun();
-//         b.stop();
-//     }
-// });
-// document.addEventListener("keydown",function(e){
-//     if(e.key === "Enter"){
-//         a.curmario = "./asset/mario1_move0.png";
-//         b.run();
-//     }
-// });
+document.querySelector("#ready").addEventListener("click", ()=>{
+    document.querySelector("#ready").style.display = "none";
+    document.querySelector("#mario").style.display = "block";
 
-//xử lý tín hiệu nhận input
-let inputTextTag = document.querySelector("#input").querySelector("input");
-inputTextTag.focus();
-//xử lý koopa
-var b = new Koopa();
-//loop
+    //xử lý tín hiệu nhận input
+    let inputTextTag = document.querySelector("#input").querySelector("input");
+    inputTextTag.focus();
+    //xử lý koopa
+    var b = new Koopa();
+    //loop
 
-let count = 0;
-let countDisplayGun = 0;
-var isExplode = false;
-let isDisplayGun = false;
+    let count = 0;
+    let countDisplayGun = 0;
+    var isExplode = false;
+    let isDisplayGun = false;
 
-let mainInterval = setInterval(()=>{
-    //vòng for tần số cao
-    //mario chạy
-    a.renderRun();
-    //xử lý string input
-    var stringInput = inputTextTag.value;
-    if(stringInput.length > 3){
-        var len = stringInput.length;
-        stringInput = inputTextTag.value.substring(len-3, len);
-    }
-    //check từ đang xét
-    if(stringInput.includes(b.queue[0])){
-        isDisplayGun = true;
-        isExplode = true;
-        b.queue.shift();
-        inputTextTag.value = ""; 
-
-    }
-    if(countDisplayGun <= 5 && isDisplayGun == true){
-        a.renderMarioGun();
-        countDisplayGun++;
-        playshotmusic();
-        if(isExplode ==true){
-            document.querySelectorAll(".kpEle")[0].querySelector("img").src = "./asset/explode.gif";
-            isExplode = false;
-            document.querySelectorAll(".kpEle")[0].querySelector("div").textContent = "";
+    let mainInterval = setInterval(()=>{
+        //vòng for tần số cao
+        //mario chạy
+        a.renderRun();
+        //xử lý string input
+        var stringInput = inputTextTag.value;
+        if(stringInput.length > 3){
+            var len = stringInput.length;
+            stringInput = inputTextTag.value.substring(len-3, len);
         }
-    }else if(a.curmario == "./asset/mariogun2.png"){
-        a.curmario = "./asset/mario1_move0.png";
-        countDisplayGun = 0;
-        isDisplayGun = false;
-        document.querySelectorAll(".kpEle")[0].remove();
-    }
-    //vòng for tần số thấp
-    if(count%30==0){
-        b.renderRun();
-    }
-    count++;
+        //check từ đang xét
+        if(stringInput.includes(b.queue[0])){
+            isDisplayGun = true;
+            isExplode = true;
+            b.queue.shift();
+            inputTextTag.value = ""; 
 
-    //kiểm tra game over
-    if(Koopa.returnGameOver() == true){
-        clearInterval(mainInterval);
-        alert("game over");
-    }
-    
-}, 100);
+        }
+        if(countDisplayGun <= 5 && isDisplayGun == true){
+            a.renderMarioGun();
+            countDisplayGun++;
+            playshotmusic();
+            if(isExplode ==true){
+                document.querySelectorAll(".kpEle")[0].querySelector("img").src = "./asset/explode.gif";
+                isExplode = false;
+                document.querySelectorAll(".kpEle")[0].querySelector("div").textContent = "";
+            }
+        }else if(a.curmario == "./asset/mariogun2.png"){
+            a.curmario = "./asset/mario1_move0.png";
+            countDisplayGun = 0;
+            isDisplayGun = false;
+            document.querySelectorAll(".kpEle")[0].remove();
+        }
+        //vòng for tần số thấp
+        if(count%30==0){
+            b.renderRun();
+        }
+        count++;
+
+        //kiểm tra game over
+        if(Koopa.returnGameOver() == true){
+            a.marioDead();
+            clearInterval(mainInterval);
+        }
+        
+    }, 100);
+
+})
+
 
 function playbgmusic(){
     var a = document.createElement("audio");
